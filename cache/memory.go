25d820706926577428c63c16f8e461a9b6670332
Brendan Horng <brendan.horng@ludusamo.com>
2024-01-17 20:46:44 +0000

    feat: make in memory store thread safe

--- cache/memory.go
+++ cache/memory.go
@@ -1,12 +1,16 @@
 package cache
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 type InMemoryStore struct {
 	data       map[string]*Data
 	sizeCached int
 	maxCached  int
 	tracker    *lruTracker
+	lock       *sync.RWMutex
 }
 
 func NewInMemoryStore(maxCached int) *InMemoryStore {
@@ -15,17 +19,31 @@ func NewInMemoryStore(maxCached int) *InMemoryStore {
 	s.sizeCached = 0
 	s.maxCached = maxCached
 	s.tracker = newLruTracker()
+	s.lock = new(sync.RWMutex)
 	return &s
+}
+
+func (s *InMemoryStore) put(topic string, data *Data) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	s.data[topic] = data
+}
+
+func (s *InMemoryStore) get(topic string) (*Data, bool) {
+	s.lock.RLock()
+	defer s.lock.RUnlock()
+	d, found := s.data[topic]
+	return d, found
 }
 
 func (s *InMemoryStore) Store(topic string, data *Data) bool {
 	fmt.Println("storing in memory")
-	s.data[topic] = data
+	s.put(topic, data)
 	s.sizeCached += data.Meta.Size
 	for s.sizeCached > s.maxCached {
 		fmt.Printf("%d is > %d maxCached\n", s.sizeCached, s.maxCached)
 		node := s.tracker.pop()
-		lru, found := s.data[node.topic]
+		lru, found := s.get(node.topic)
 		if found {
 			fmt.Printf("deleting %s from memory for %d\n", node.topic, lru.Meta.Size)
 			s.Delete(node.topic)
@@ -37,15 +55,17 @@ func (s *InMemoryStore) Store(topic string, data *Data) bool {
 
 func (s *InMemoryStore) Retrieve(topic string) (*Data, bool) {
 	fmt.Println("retrieving in memory")
-	data, found := s.data[topic]
+	data, found := s.get(topic)
 	s.tracker.use(topic)
 	return data, found
 }
 
 func (s *InMemoryStore) Delete(topic string) bool {
-	data, found := s.data[topic]
+	data, found := s.get(topic)
 	if found {
 		s.sizeCached -= data.Meta.Size
+		s.lock.Lock()
+		defer s.lock.Unlock()
 		delete(s.data, topic)
 	}
 	return true
@@ -54,6 +74,8 @@ func (s *InMemoryStore) Delete(topic string) bool {
 func (s *InMemoryStore) List() []string {
 	topics := make([]string, len(s.data))
 	i := 0
+	s.lock.RLock()
+	defer s.lock.RUnlock()
 	for k := range s.data {
 		topics[i] = k
 		i++
